38fd50492a0c5ec3b9b8f13dc0cc68a2581239d8
RotaruDan <drotaru@ucm.es>
2015-07-10 18:05:16 +0200

    Some small format improvements.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,43 +1,4 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "¡No se han proporcionado argumentos!";
-	return false;
-   }
-
-if(isset($_POST['g-recaptcha-response'])){
- 	if(!verify($_POST['g-recaptcha-response'])) {
-		echo "La comprobación NoCAPTCHA es incorrecta";
-		return false;
- 	}
-} else {	
-	echo "¡No se han proporcionado captcha!";
-	return false;
-}
-
-	echo "¡No se han proporcionado captcha!";
-	return false;
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'testresidenciamoncloa@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Formulario de contacto de Residenciamoncloa:  $name";
-$email_body = "Has recibido un nuevo mensaje de tu formulario de contacto de la web.\n\n"."Aquí tienes los detalles:\n\nNomnre: $name\n\nEmail: $email_address\n\nPteléfono: $phone\n\nMensaje:\n$message";
-$headers = "From: noreply@residenciamoncloa.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;	
-
-
 
 function verify($response) {
 	$ip = $_SERVER['REMOTE_ADDR']; //server Ip
@@ -54,5 +15,40 @@ function verify($response) {
 		return True;
 	}
 	return False;
-}		
+}	
+
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+		echo "¡No se han proporcionado argumentos!";
+		return false;
+   }
+
+if(isset($_POST['g-recaptcha-response'])){
+ 	if(!verify($_POST['g-recaptcha-response'])) {
+		echo "La comprobación NoCAPTCHA es incorrecta";
+		return false;
+ 	}
+} else {	
+	echo "¡No se han proporcionado captcha!";
+	return false;
+}
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'testresidenciamoncloa@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulario de contacto de Residenciamoncloa:  $name";
+$email_body = "Has recibido un nuevo mensaje de tu formulario de contacto de la web.\n\n"."Aquí tienes los detalles:\n\nNomnbre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
+$headers = "From: noreply@residenciamoncloa.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+
+return mail($to,$email_subject,$email_body,$headers);
+	
 ?>
